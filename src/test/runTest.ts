f17fd3792c17a042552fc92b51f48416cd6e9dbb
Neil <nmsmith22389@gmail.com>
2020-08-18 12:14:51 -0500

    ci(travis): fix testing for travis ci(travis): fix testing for travis
    again...

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,19 +1,24 @@
 import * as path from 'path';
-
 import { runTests } from 'vscode-test';
 
 async function main(): Promise<void> {
     try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
-        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+        const extensionDevelopmentPath = process.cwd();
 
         // The path to test runner
         // Passed to --extensionTestsPath
-        const extensionTestsPath = path.resolve(__dirname, './suite/index');
+        const extensionTestsPath = path.resolve(extensionDevelopmentPath, './out/test/suite');
+
+        const demoFile = path.resolve(extensionDevelopmentPath, './src/test/demos/demo.js');
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: ['./demos'] });
+        await runTests({
+            extensionDevelopmentPath,
+            extensionTestsPath,
+            launchArgs: ['--disable-extensions', demoFile],
+        });
     } catch (err) {
         console.error(err);
         console.error('Failed to run tests');
